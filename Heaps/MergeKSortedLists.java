8fe4eb552dc25e9f9c0953a684865a3041f7eff9
Chandan Troughia <chandantroughia.com>
2018-01-15 13:35:47 -0500

    11th commit

---
+++ Heaps/MergeKSortedLists.java
@@ -0,0 +1,47 @@
+package Heaps;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class MergeKSortedLists {
+
+    public class ListNode{
+        int val;
+        ListNode next;
+        public ListNode(int x){
+            this.val = x;
+        }
+    }
+    
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists==null||lists.length==0)
+            return null;
+
+        PriorityQueue<ListNode> queue = new PriorityQueue<ListNode>(new Comparator<ListNode>(){
+            public int compare(ListNode l1, ListNode l2){
+                return l1.val - l2.val;
+            }
+        });
+
+        ListNode head = new ListNode(0);
+        ListNode p = head;
+
+        for(ListNode list: lists){
+            if(list!=null)
+                queue.offer(list);
+        }
+
+        while(!queue.isEmpty()){
+            ListNode n = queue.poll();
+            p.next = n;
+            p=p.next;
+
+            if(n.next!=null)
+                queue.offer(n.next);
+        }
+
+        return head.next;
+
+    }
+}
